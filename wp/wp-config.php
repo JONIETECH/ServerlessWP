82c16b8d74a9b596dda03f6922e034d77719e28f
RWOTHOMIO JONATHAN <jonahbst@gmail.com>
2024-11-08 19:27:56 +0300

    Update wp-config.php

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -2,42 +2,21 @@
 /**
  * The base configuration for WordPress
  *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the web site, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * Database settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://wordpress.org/documentation/article/editing-wp-config-php/
- *
  * @package WordPress
  */
 
-// ** Database settings - You can get this info from your web host ** //
+// ** Database settings ** //
 /** The name of the database for WordPress */
-if (isset($_ENV['DATABASE'])) {
-  define( 'DB_NAME', $_ENV['DATABASE'] );
-}
+define( 'DB_NAME', getenv('DATABASE') ?: 'database_name' );
 
 /** Database username */
-if (isset($_ENV['USERNAME'])) {
-  define( 'DB_USER', $_ENV['USERNAME'] );
-}
+define( 'DB_USER', getenv('USERNAME') ?: 'username' );
 
 /** Database password */
-if (isset($_ENV['PASSWORD'])) {
-  define( 'DB_PASSWORD', $_ENV['PASSWORD'] );
-}
+define( 'DB_PASSWORD', getenv('PASSWORD') ?: 'password' );
 
 /** Database hostname */
-if (isset($_ENV['HOST'])) {
-  define( 'DB_HOST', $_ENV['HOST'] );
-}
+define( 'DB_HOST', getenv('HOST') . ':' . getenv('PORT') ?: 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -47,67 +26,41 @@ define( 'DB_COLLATE', '' );
 
 /**#@+
  * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
+ * Generate new keys using the WordPress.org secret-key service.
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         'your-unique-phrase' );
+define( 'SECURE_AUTH_KEY',  'your-unique-phrase' );
+define( 'LOGGED_IN_KEY',    'your-unique-phrase' );
+define( 'NONCE_KEY',        'your-unique-phrase' );
+define( 'AUTH_SALT',        'your-unique-phrase' );
+define( 'SECURE_AUTH_SALT', 'your-unique-phrase' );
+define( 'LOGGED_IN_SALT',   'your-unique-phrase' );
+define( 'NONCE_SALT',       'your-unique-phrase' );
 
 /**#@-*/
 
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix = isset($_ENV['TABLE_PREFIX']) ? $_ENV['TABLE_PREFIX'] : 'wp_';
+/** WordPress database table prefix. */
+$table_prefix = getenv('TABLE_PREFIX') ?: 'wp_';
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
- */
+/** Enable debugging for development */
 define( 'WP_DEBUG', false );
 
-/* Add any custom values between this line and the "stop editing" line. */
-
+/** Enforce SSL */
 define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL );
 $_SERVER['HTTPS'] = 'on';
 
-// Optional S3 credentials for file storage.
-if (isset($_ENV['S3_KEY_ID']) && isset($_ENV['S3_ACCESS_KEY'])) {
+// Optional S3 credentials for external file storage, such as AWS S3.
+if (getenv('S3_KEY_ID') && getenv('S3_ACCESS_KEY')) {
 	define( 'AS3CF_SETTINGS', serialize( array(
         'provider' => 'aws',
-        'access-key-id' => $_ENV['S3_KEY_ID'],
-        'secret-access-key' => $_ENV['S3_ACCESS_KEY'],
-) ) );
+        'access-key-id' => getenv('S3_KEY_ID'),
+        'secret-access-key' => getenv('S3_ACCESS_KEY'),
+    ) ) );
 }
 
-// Disable file modification because the changes won't be persisted.
-define('DISALLOW_FILE_EDIT', true );
-define('DISALLOW_FILE_MODS', true );
-
-/* That's all, stop editing! Happy publishing. */
+/** Disable file modifications on serverless platforms */
+define('DISALLOW_FILE_EDIT', true);
+define('DISALLOW_FILE_MODS', true);
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
